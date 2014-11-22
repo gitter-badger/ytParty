8bbe5af8889b8238edac207f6b1e2ff4674beb56
Kasjan Siwek <kasjan.siwek@gmail.com>
2014-11-22 18:47:50 +0100

    Catchup

--- ytParty/queue/urls.py
+++ ytParty/queue/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from queue import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^player/$', views.index),
-    url(r'^player/(?P<party_token>\w+)/$', views.player, name='player'),
+    url(r'^$', views.index_view, name='index'),
+    url(r'^player/$', views.index_view),
+    url(r'^player/(?P<party_token>\w+)/$', views.player_view, name='player'),
 )
--- ytParty/queue/views.py
+++ ytParty/queue/views.py
@@ -6,11 +6,14 @@ from django.shortcuts import render, redirect
 from models import Party
 
 
-def index(request):
+def _create_and_or_get_user():
+    pass
+
+def index_view(request):
     template = loader.get_template('queue/index.html')
     return HttpResponse(template.render(RequestContext(request)))
 
-def player(request, party_token=None):
+def player_view(request, party_token=None):
     try:
         party = Party.objects.get(token=party_token)
         context = RequestContext(request, {
