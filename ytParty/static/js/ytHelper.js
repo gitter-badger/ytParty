b74cfe4be56563a8da7f49d647f557f054ab1ce0
LogvinovLeon <leonid.logvinov@codilime.com>
2014-11-23 09:22:26 +0100

    make all syncronous

--- ytParty/static/js/ytHelper.js
+++ ytParty/static/js/ytHelper.js
@@ -2,10 +2,14 @@
  * Created by leonid <> on 23.11.14.
  */
 $.youtubeAppend = function(token, destinaton_id) {
-    $.getJSON('http://gdata.youtube.com/feeds/api/videos/' + token + '?v=2&alt=jsonc', function(resp, status, xhr) {
-        $('#' + destinaton_id).append('<div class="youtube">\
+    resp = $.ajax({
+        type: "GET",
+        url: 'http://gdata.youtube.com/feeds/api/videos/' + token + '?v=2&alt=jsonc',
+        async: false
+    }).responseText;
+    resp = JSON.parse(resp);
+    $('#' + destinaton_id).append('<div class="youtube">\
                  <img src="' + resp.data.thumbnail.sqDefault + '" alt="" />\
                  <h3><a href="javascript:void(0)" onclick="$.youtubePlay(\'' + resp.data.id + '\')">' + resp.data.title + '</a></h3>\
                  </div>');
-    });
-}
+};
